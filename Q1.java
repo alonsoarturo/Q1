752224142648cad6100d106f959e94b332bcb8d0
Arturo Alonso <alonsoarturo@ou.edu>
2019-09-23 14:34:27 -0500

    Initial Commit

---
+++ Q1.java
@@ -0,0 +1,12 @@
+
+public class Q1 {
+	// Q1: Is this a valid Java Project
+	// A1: yes
+	public int main () {
+		return 10;
+	}
+		
+	public static void main(String[] args) {
+		int numb = main();
+	}
+}
