2b6249d3bfc3ca3cd67c0138debd04b5d518e65e
Arturo Alonso <alonsoarturo@ou.edu>
2019-09-26 17:54:02 -0500

    added Q1.java

--- Q1.java
+++ Q1.java
@@ -2,11 +2,13 @@
 public class Q1 {
 	// Q1: Is this a valid Java Project
 	// A1: yes
+	// myQ: 
 	public int main () {
 		return 10;
 	}
 		
 	public static void main(String[] args) {
-		int numb = main();
+		int x = 2;
+		System.out.println(x);
 	}
 }
